dd9afba23d41c1b81fc4f195ba508593704b5311
KosenkoAleksandra <kosenkoyes@gmail.com>
2023-06-25 20:28:37 +0300

    Первый коммит

---
+++ src/test/java/coursework/web_development/easyauction/EasyAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework.web_development.easyauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
